15cf9c021f1f98e91cc7724051f3b4b1337666b5
Marco Alejandro Gomez Cabrera <marco@MacBook-Pro-de-Marco.local>
2024-01-18 22:11:34 -0600

    little fix on footer

--- front/src/Components/Footer/Footer.tsx
+++ front/src/Components/Footer/Footer.tsx
@@ -3,7 +3,31 @@ import "./footer.css"
 export const Footer = () =>{
     return(
         <footer>
-                
+            <div className="footer-main-container">
+                <div className="footer-info-container">
+                    <p>
+                        Bienvenido al prototipo del sistema de organización
+                        de horarios universitarios  
+                    </p>
+                    <p>
+                        2024, Ciudad de México, todos los derechos reservados
+                    </p>
+                </div>
+                <div className="footer-icons-container">
+                    <div className="footer-icon">
+
+                    </div>
+                    <div className="footer-icon">
+                        
+                    </div>
+                    <div className="footer-icon">
+                        
+                    </div>
+                    <div className="footer-icon">
+                        
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
